cc432df1074ffaf7a897b12e4c28aa1e0bc1c70a
Yizhou Ma <yizhouma@outlook.com>
2023-07-26 09:17:42 -0400

    23.7.26

--- sketch.jl
+++ sketch.jl
@@ -4,16 +4,27 @@ include("gaussian_elimination.jl")
 include("dynamic_update.jl")
 
 
-function test(n)
-    a=zeros(Bool,(n,n))
-    b=ones(Bool,(n,n))
-    c=rand(Bool,(n,n))
+n_stab=4
+dof_size=4
 
-    # println(gaussian_elimination!(a,b,false))
-    println(gaussian_elimination!(c,a,false))
-    println(c)
-    
+stab_generators=rand(Bool,(n_stab,dof_size)) # The first index is the index of the stabilizer. The second index is the index of the d.o.f.
+stab_generator_recombined=zeros(Bool,(n_stab,dof_size)) # For each stabilizer the first index is the index of the stabilizer. The second index is the index of the d.o.f.
 
+recomb_mat=zeros(Bool,(n_stab,n_stab)) # A rectangular pivotal matrix. The size is n_stab, the number of stabilizers.
+for i in 1:n_stab
+    recomb_mat[i,i]=true
+    recomb_mat[i,i+1:n_stab]=rand(Bool,n_stab-i)
 end
 
-test(4)
+stab_generator_recombined=zeros(Bool,(n_stab,dof_size)) # For each stabilizer the first index is the index of the stabilizer. The second index is the index of the d.o.f.
+for i in 1:n_stab
+    for j in 1:n_stab
+        if(recomb_mat[i,j])
+            @views stab_generator_recombined[i,:]=stab_generator_recombined[i,:] .⊻ stab_generators[j,:]
+        end
+    end
+end
+
+println("stab_generators:",stab_generators)
+println("recomb_mat:",recomb_mat)
+println("stab_generator_recombined:",stab_generator_recombined)
--- test.jl
+++ test.jl
@@ -135,6 +135,153 @@ function toric_code_static(l::Int64,Parallel::Bool)
     # plot_and_fit_native(entropy_arr,volume_arr,area_arr)
 end
 
+function toric_code_static_recombination(l::Int64,Parallel::Bool)
+    # Note that the d.o.f. are on the edges, not on the vertices.
+    # The stabilizers are ordered as X1,Z1,X2,Z2,...,Xn,Zn.
+
+    lattice_size::Int64=2*l*l
+
+    # For a lattice of length l, the size is 2*l*l, since each vertex corresponds to two edges.
+    # There is one stabilizer on each vertex and each plaquette, so the total number of stabilizers is 2*l*l, i.e. lattice_size.
+
+    stab_generators=zeros(Bool,(lattice_size,2*lattice_size)) # For each stabilizer the first index is the index of the stabilizer. The second index is the index of the d.o.f.
+
+    # Vertex Stabilizers
+    for m in 1:l*l # m is the index of the vertex
+        y=m%l
+        if y==0
+            y=l
+        end
+        x=Int64((m-y)/l)+1
+
+        # Relation between the coordinate and the index of the vertex: m=(x-1)*l+y
+        # The edge on the right of the vertex m is 2*m-1. The edge on the down of the vertex is 2*m.
+
+        x_left=x-1
+        if x_left==0
+            x_left=l
+        end
+        m_left=(x_left-1)*l+y
+
+        y_up=y-1
+        if y_up==0
+            y_up=l
+        end
+        m_up=(x-1)*l+y_up
+
+        stab_generators[m,2*(2*m-1)]=true
+        stab_generators[m,2*(2*m)]=true
+        stab_generators[m,2*(2*m_left-1)]=true
+        stab_generators[m,2*(2*m_up)]=true
+        #=
+        Each vertex stabilizer acts on 4 edges as Pauli_Z:
+        Right edge, 2*m-1; 
+        Down edge, 2*m; 
+        Left edge (or right edge of vertex on the left). The coordinate of the vertex on the left is (x-1 (l if equals zero),y), 
+        Up edge (or down edge of vertex on the top), The coordinate of the vertex on the top is (x,y-1 (l if equals zero)).
+        2*n denotes Pauli_Z on the n-th d.o.f.
+        =#
+    end
+
+    # Plaquette Stabilizers
+    for m in 1:l*l # Consider the plaquette at the downright of the vertex in question.
+        y=m%l
+        if y==0
+            y=l
+        end
+            
+        x=Int64((m-y)/l)+1
+
+        y_down=y%l+1
+        x_right=x%l+1
+        
+        m_down=(x-1)*l+y_down
+        m_right=(x_right-1)*l+y
+
+        stab_generators[l*l+m,2*(2*m-1)-1]=true
+        stab_generators[l*l+m,2*(2*m)-1]=true
+        stab_generators[l*l+m,2*(2*m_down-1)-1]=true
+        stab_generators[l*l+m,2*(2*m_right)-1]=true
+        #=
+        Each vertex stabilizer acts on 4 edges: 
+        Right edge, 2*m-1; 
+        Down edge, 2*m; 
+        Right edge of the vertex on the downside. The coordinate of the vertex on the downside is (x,y%l+1).
+        Down edge of the vertex on the right. The coordinate of the vertex on the right is (x%l+1,y).
+    
+        2*n-1 denotes Pauli_X on the n-th d.o.f.
+        =#
+    end
+
+    println("toric code recombination")
+    println(length(stab_generators[:,1]))
+    # println(stab_generators)
+
+    # Generate Pivotal Matrices
+    recomb_mat=zeros(Bool,(lattice_size,lattice_size)) # A rectangular pivotal matrix. The size is lattice_size, the number of stabilizers.
+    for i in 1:lattice_size
+        recomb_mat[i,i]=true
+        recomb_mat[i,i+1:lattice_size]=rand(Bool,lattice_size-i)
+    end
+
+    stab_generator_recombined=zeros(Bool,(lattice_size,2*lattice_size)) # For each stabilizer the first index is the index of the stabilizer. The second index is the index of the d.o.f.
+    for i in 1:lattice_size
+        for j in 1:lattice_size
+            if(recomb_mat[i,j])
+                @views stab_generator_recombined[i,:]=stab_generator_recombined[i,:] .⊻ stab_generators[j,:]
+            end
+        end
+    end
+
+    # Select rectangular regions with increasing sizes and plot entropy vs region size.
+
+    start=Int64(floor(l/5))
+
+    volume_arr=zeros(Int64,Int64(l/2)-start+1)
+    area_arr=zeros(Int64,Int64(l/2)-start+1)
+    entropy_arr=zeros(Float32,Int64(l/2)-start+1)
+
+    region=zeros(Int64,(Int64(l/2)-start+1,2*Int64(l/2)^2))
+    cur=zeros(Int64,Int64(l/2)-start+1)
+
+    for s in range(max(start,2),Int64(l/2))
+        cur[s-start+1]=1
+        area_arr[s-start+1]=4*s
+
+        for i in 1:s
+            for j in 1:s
+                m=(i-1)*l+j # The vertex
+                if(i<s) # Not on the rightmost
+                    region[s-start+1,cur[s-start+1]]=2*m-1
+                    cur[s-start+1]+=1
+                    volume_arr[s-start+1]+=1
+                end
+
+                if(j<s) # Not on the downmost
+                    region[s-start+1,cur[s-start+1]]=2*m
+                    volume_arr[s-start+1]+=1
+                    cur[s-start+1]+=1
+                end
+            end
+        end
+
+        # println(region[s-start+1,:]) # Only for debugging
+        # entropy_arr[s-start+1]=calc_entropy(stab_generators,region[s-start+1,1:cur-1],Parallel)
+    end
+
+
+    for s in range(max(start,2),Int64(l/2)) # The line is separated for testing the performance of the function.
+        entropy_arr[s-start+1]=calc_entropy(stab_generator_recombined,view(region,s-start+1,:),Parallel,cur[s-start+1]-1)
+    end
+
+    println("area_arr:",area_arr)
+    println("entropy_arr:",entropy_arr)
+    ## plot_and_fit(entropy_arr,volume_arr,area_arr) 
+        # Disabled when timing.
+    fit(entropy_arr,volume_arr,area_arr)
+    # plot_and_fit_native(entropy_arr,volume_arr,area_arr)
+end
+
 function toric_code_static_multisampling(l::Int64,Parallel::Bool)
     # Note that the d.o.f. are on the edges, not on the vertices.
     # The stabilizers are ordered as X1,Z1,X2,Z2,...,Xn,Zn.
@@ -427,14 +574,8 @@ function test_dynamic_update_performance(LatticeSize::Int64,ISGSize::Int64,cycle
 end
 
 
-single_hexagon_3rounds(2,false,false)
-println("False False")
-single_hexagon_3rounds(2,true,false)
-println("True False")
-single_hexagon_3rounds(2,false,true)
-println("False True")
-single_hexagon_3rounds(2,true,true)
-println("True True")
+toric_code_static_recombination(24,true)
+
 
 # test_dynamic_update_performance(2,2)
 # test_dynamic_update_performance(4096,1024)
