9a84a87e87c83333bab72cdbb966123dcd9da5c0
Yizhou Ma <yizhouma@outlook.com>
2023-09-12 11:01:15 +0800

    Add title, separate initialization

--- cluster_measure_bulk.jl
+++ cluster_measure_bulk.jl
@@ -2,7 +2,10 @@ include("utils.jl")
 include("dynamic_update.jl")
 import PyPlot
 
-function plot_stabilizer_generators(Width::Int,Depth::Int,Stabilizers::Array{Bool,2},PlotNumber::Int)
+
+plot_round=4
+
+function plot_stabilizer_generators(Width::Int,Depth::Int,Stabilizers::Array{Bool,2},PlotNumber::Int,Title::String)
 
     # Use PyPlot library to visualize the stabilizer generators on a square lattice and show a graph for each stabilizer
     # A vertex is green if there is a Pauli-X, blue if a Pauli-Z, and red if both.
@@ -25,9 +28,9 @@ function plot_stabilizer_generators(Width::Int,Depth::Int,Stabilizers::Array{Boo
             if(Stabilizers[j,2*n-1] && Stabilizers[j,2*n])
                 color[n]="red"
             elseif(Stabilizers[j,2*n-1])
-                color[n]="green"
+                color[n]="gold" # Pauli-X
             elseif(Stabilizers[j,2*n])
-                color[n]="blue"
+                color[n]="blue" # Pauli-Z
             else
                 color[n]="grey"
             end
@@ -39,6 +42,7 @@ function plot_stabilizer_generators(Width::Int,Depth::Int,Stabilizers::Array{Boo
         PyPlot.ylim(0,Depth+1)
         PyPlot.xticks([])
         PyPlot.yticks([])
+        PyPlot.title(Title)
         PyPlot.show()
     end
 
@@ -60,39 +64,7 @@ function rough_boundary_X(Width::Int,Depth::Int)
 
     # Initialize the stabilizer group.
 
-    for y in 1:Depth
-        for x in 1:Width
-            n=Width*(y-1)+x
-            Initial_SG[n,2*n-1]=true # Pauli-X
-            
-            if(y>1)
-                n_down=Width*(y-2)+x
-                Initial_SG[n,2*n_down]=true # Pauli-Z
-            end
-
-            if(y<Depth)
-                n_up=Width*y+x
-                Initial_SG[n,2*n_up]=true # Pauli-Z
-
-                if(x>1) # The upper boundary is rough, so at the upmost there is not left and right neighbors.
-                    n_left=n-1
-                    Initial_SG[n,2*n_left]=true # Pauli-Z
-                else
-                    n_left=Width*(y-1)+Width #leftmost
-                    Initial_SG[n,2*n_left]=true # Pauli-Z
-                end
-
-                if(x<Width)
-                    n_right=Width*(y-1)+x+1
-                    Initial_SG[n,2*n_right]=true # Pauli-Z
-                else
-                    n_right=Width*(y-1)+1 # rightmost
-                    Initial_SG[n,2*n_right]=true # Pauli-Z
-                end
-            end
-
-        end
-    end
+    initialize_rough_boundary(Width,Depth,Initial_SG)
 
 
     # Initialize Measurements
@@ -113,9 +85,9 @@ function rough_boundary_X(Width::Int,Depth::Int)
     
     println(ISG_to_string(new_SG,1,Width,Depth,true))
 
-    plot_stabilizer_generators(Width,Depth,new_SG,4)
+    title="Rough Boundary X,Width="*string(Width)*",Depth="*string(Depth)
+    plot_stabilizer_generators(Width,Depth,new_SG,plot_round,title)
 end
-
 
 function smooth_boundary_Y(Width::Int,Depth::Int)
     # The lattice is put on a cylinder, i.e. a periodic boundary condition in the x direction and an open boundary condition in the y direction.
@@ -131,41 +103,7 @@ function smooth_boundary_Y(Width::Int,Depth::Int)
     measurements=zeros(Bool,Width*(Depth-1),2*lattice_size) # The lowest row isn't measured.
 
     # Initialize the stabilizer group.
-
-    for y in 1:Depth
-        for x in 1:Width
-            n=Width*(y-1)+x
-            Initial_SG[n,2*n-1]=true # Pauli-X
-            
-            if(y>1)
-                n_down=Width*(y-2)+x
-                Initial_SG[n,2*n_down]=true # Pauli-Z
-            end
-
-            if(y<Depth)
-                n_up=Width*y+x
-                Initial_SG[n,2*n_up]=true # Pauli-Z
-            end
-
-            if(x>1) # The upper boundary is smooth, so every point has left and right neighbors.
-                n_left=n-1
-                Initial_SG[n,2*n_left]=true # Pauli-Z
-            else
-                n_left=Width*(y-1)+Width #leftmost
-                Initial_SG[n,2*n_left]=true # Pauli-Z
-            end
-
-            if(x<Width)
-                n_right=Width*(y-1)+x+1
-                Initial_SG[n,2*n_right]=true # Pauli-Z
-            else
-                n_right=Width*(y-1)+1 # rightmost
-                Initial_SG[n,2*n_right]=true # Pauli-Z
-            end
-
-        end
-    end
-
+    initialize_smooth_boundary(Width,Depth,Initial_SG)
 
     # Initialize Measurements
     for y in 2:Depth
@@ -188,13 +126,136 @@ function smooth_boundary_Y(Width::Int,Depth::Int)
     println("New_SG")
     println(size(new_SG))
     println(ISG_to_string(new_SG,1,Width,Depth,true))
-    plot_stabilizer_generators(Width,Depth,new_SG,4)
+
+    title="Smooth Boundary Y,Width="*string(Width)*",Depth="*string(Depth)
+    plot_stabilizer_generators(Width,Depth,new_SG,plot_round,title)
 
 end
 
-smooth_boundary_Y(6,8)
-smooth_boundary_Y(6,10)
-smooth_boundary_Y(6,12)
+function rough_boundary_Y(Width::Int,Depth::Int)
+    # The lattice is put on a cylinder, i.e. a periodic boundary condition in the x direction and an open boundary condition in the y direction.
+    # The lower boundary is a smooth boundary and the upper boundary is a rough boundary.
+        # The lower boundary has y coordinate 1. The upper boundary 
+
+    # The function calculates the boundary stabilizers after measuring all single-site X in the bulk.
+    
+    # Initially, the state is a cluster state.
+
+    lattice_size=Width*Depth
+    Initial_SG=zeros(Bool,lattice_size,2*lattice_size) # One stabilizer each site and we need two elements for each site.
+    measurements=zeros(Bool,Width*(Depth-1),2*lattice_size) # The lowest row isn't measured.
+
+    # Initialize the stabilizer group.
+    initialize_rough_boundary(Width,Depth,Initial_SG)
+
+    # Initialize Measurements
+    for y in 2:Depth
+        for x in 1:Width
+            n=Width*(y-1)+x
+
+            # Y-measurements
+            measurements[n-Width,2*n-1]=true
+            measurements[n-Width,2*n]=true 
+        end
+    end
+
+    # Update the stabilizer group
+    println("Initial_SG")
+    println(size(Initial_SG))
+    # println(ISG_to_string(Initial_SG,1,Width,Depth,true))
+    # plot_stabilizer_generators(Width,Depth,Initial_SG)
+
+    new_SG=update(lattice_size,Initial_SG,measurements)
+    println("New_SG")
+    println(size(new_SG))
+    println(ISG_to_string(new_SG,1,Width,Depth,true))
+
+    title="Rough Boundary Y,Width="*string(Width)*",Depth="*string(Depth)
+    plot_stabilizer_generators(Width,Depth,new_SG,plot_round,title)
+
+end
+
+
+
+function initialize_smooth_boundary(Width::Int,Depth::Int,StabArray::Array{Bool,2})
+    for y in 1:Depth
+        for x in 1:Width
+            n=Width*(y-1)+x
+            StabArray[n,2*n-1]=true # Pauli-X
+            
+            if(y>1)
+                n_down=Width*(y-2)+x
+                StabArray[n,2*n_down]=true # Pauli-Z
+            end
+
+            if(y<Depth)
+                n_up=Width*y+x
+                StabArray[n,2*n_up]=true # Pauli-Z
+            end
+
+            if(x>1) # The upper boundary is smooth, so every point has left and right neighbors.
+                n_left=n-1
+                StabArray[n,2*n_left]=true # Pauli-Z
+            else
+                n_left=Width*(y-1)+Width #leftmost
+                StabArray[n,2*n_left]=true # Pauli-Z
+            end
+
+            if(x<Width)
+                n_right=Width*(y-1)+x+1
+                StabArray[n,2*n_right]=true # Pauli-Z
+            else
+                n_right=Width*(y-1)+1 # rightmost
+                StabArray[n,2*n_right]=true # Pauli-Z
+            end
+
+        end
+    end
+end
+
+
+function initialize_rough_boundary(Width::Int,Depth::Int,StabArray::Array{Bool,2})
+    for y in 1:Depth
+        for x in 1:Width
+            n=Width*(y-1)+x
+            StabArray[n,2*n-1]=true # Pauli-X
+            
+            if(y>1)
+                n_down=Width*(y-2)+x
+                StabArray[n,2*n_down]=true # Pauli-Z
+            end
+
+            if(y<Depth)
+                n_up=Width*y+x
+                StabArray[n,2*n_up]=true # Pauli-Z
+
+                if(x>1) # The upper boundary is rough, so no left or right neighbors.
+                    n_left=n-1
+                    StabArray[n,2*n_left]=true # Pauli-Z
+                else
+                    n_left=Width*(y-1)+Width #leftmost
+                    StabArray[n,2*n_left]=true # Pauli-Z
+                end
+    
+                if(x<Width)
+                    n_right=Width*(y-1)+x+1
+                    StabArray[n,2*n_right]=true # Pauli-Z
+                else
+                    n_right=Width*(y-1)+1 # rightmost
+                    StabArray[n,2*n_right]=true # Pauli-Z
+                end
+            end
+
+
+
+        end
+    end
+end
+
+
+smooth_boundary_Y(10,21)
+smooth_boundary_Y(10,25)
+smooth_boundary_Y(10,29)
 
 
 
